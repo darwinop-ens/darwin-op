326c60409673fff8f2885ca4f1b97c7e47803be5
zerom97 <zerom97@5b76bc46-f30d-4be6-99a2-e548306fc9d2>
2011-05-24 10:28:54 +0000

    MX-28 firmware updated.
    git-svn-id: http://darwinop.svn.sourceforge.net/svnroot/darwinop/trunk@59 5b76bc46-f30d-4be6-99a2-e548306fc9d2

--- darwin/Linux/project/firmware_installer/main.cpp
+++ darwin/Linux/project/firmware_installer/main.cpp
@@ -91,7 +91,7 @@ void help(char *progname)
                     " [-h | --help]........: display this help\n" \
                     " [-d | --device]......: port to open                    (/dev/ttyUSB0)\n" \
                     " [-c | --controller]..: controller firmware file      (cm730_0x12.hex)\n" \
-                    " [-a | --actuator]....: actuator firmware file    (mx28_0x1C_4096.hex)\n", progname);
+                    " [-a | --actuator]....: actuator firmware file    (mx28_0x1D_4096.hex)\n", progname);
     fprintf(stderr, "-----------------------------------------------------------------------\n");
     fprintf(stderr, "Example #1:\n" \
                     " To open a default port and install with firmware file \"cm730.hex\":\n" \
@@ -364,7 +364,7 @@ int main(int argc, char *argv[])
     fprintf(stderr,   "***********************************************************************\n\n");
 
     char *controller_fw = (char*)"cm730_0x12.hex";
-    char *actuator_fw = (char*)"mx28_0x1C_4096.hex";
+    char *actuator_fw = (char*)"mx28_0x1D_4096.hex";
     char *dev = (char*)"/dev/ttyUSB0";
 
     /* parameter parsing */
