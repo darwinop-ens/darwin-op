e5b57d692e1fb5da5bbe1dcbcf036cad8f7d92c3
Florent Ouchet <florent.ouchet@bretagne.ens-cachan.fr>
2013-03-11 10:25:15 -0400

    it works! joystick  sound

--- Francois_projects/consoletest/src/VocalSynthesizerSpeaker.cpp
+++ Francois_projects/consoletest/src/VocalSynthesizerSpeaker.cpp
@@ -1,16 +1,18 @@
 #include "VocalSynthesizerSpeaker.h"
-
+#include <speak_lib.h>
 
 VocalSynthesizerSpeaker::VocalSynthesizerSpeaker()
 {
-    path="C:/Users/Ancmin/Desktop/consoletest";
+    //path="C:/Users/Ancmin/Desktop/consoletest";
+    //path="/usr/share";
+    path=NULL;
     Buflength = 500;
     Options = 0;
     position=0; end_position=0; flags=espeakCHARS_AUTO;
 
     output = AUDIO_OUTPUT_PLAYBACK;
-//    espeak_Initialize(output, Buflength, path, Options );
- //   espeak_SetVoiceByName("default");
+    espeak_Initialize(output, Buflength, path, Options );
+    espeak_SetVoiceByName("default");
 }
 
 VocalSynthesizerSpeaker::~VocalSynthesizerSpeaker()
@@ -24,11 +26,11 @@ VocalSynthesizerSpeaker::~VocalSynthesizerSpeaker()
   */
 void VocalSynthesizerSpeaker::say(std::string text)
 {
-  //  espeak_Synth( text.c_str(), text.size()+1, position, position_type, end_position, flags,
-//    unique_identifier, user_data );
+   espeak_Synth( text.c_str(), text.size()+1, position, position_type, end_position, flags,
+    unique_identifier, user_data );
     /*here, I MUST LEAVE A COMMENT... there were a bug: text.size() instead of text.size()+1... long life to C/C++ :)*/
 
-  //  espeak_Synchronize( );
+    espeak_Synchronize( );
 
 }
 
