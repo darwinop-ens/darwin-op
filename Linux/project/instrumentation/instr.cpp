24b1d12f8ecb1a75d0623ccc39b0a1075916070b
Florent Ouchet <florent.ouchet@bretagne.ens-cachan.fr>
2013-04-21 10:06:02 -0400

    move timeval_substract to a common unit (for sharing this function with future developments).

--- Linux/project/instrumentation/instr.cpp
+++ Linux/project/instrumentation/instr.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#include "timeutils.h"
 #include "instr.h"
 #include "LinuxDARwIn.h"
 
@@ -326,35 +327,6 @@ void InstrServer::ProcessStartCommand(string::iterator &iterator, string::iterat
 
 	if (m_print_debug)
 		cout << "instr: start Command" << endl;
-}
-
-// the following function is taken from http://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
-
-/* Subtract the `struct timeval' values X and Y,
-   storing the result in RESULT.
-   Return 1 if the difference is negative, otherwise 0. */
-     
-int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y)
-{
-	/* Perform the carry for the later subtraction by updating y. */
-	if (x->tv_usec < y->tv_usec)
-	{
-		int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-		y->tv_usec -= 1000000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_usec - y->tv_usec > 1000000) {
-		int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-		y->tv_usec += 1000000 * nsec;
-		y->tv_sec -= nsec;
-	}
-
-	/* Compute the time remaining to wait. tv_usec is certainly positive. */
-	result->tv_sec = x->tv_sec - y->tv_sec;
-	result->tv_usec = x->tv_usec - y->tv_usec;
-     
-	/* Return 1 if result is negative. */
-	return x->tv_sec < y->tv_sec;
 }
 
 void InstrServer::ProcessTimeCommand(string::iterator &iterator, string::iterator &end, string &result)
---
+++ Linux/project/instrumentation/timeutils.h
@@ -0,0 +1,9 @@
+#ifndef __TIMEUTILS_H__
+#define __TIMEUTILS_H__
+
+#include <sys/time.h>
+
+int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y);
+
+#endif // __TIMEUTILS_H__
+
