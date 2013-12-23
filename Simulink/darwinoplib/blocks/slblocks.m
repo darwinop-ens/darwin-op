361c123663070fb068f0d81612d4db0c25a56511
Florent Ouchet <florent.ouchet@bretagne.ens-cachan.fr>
2013-12-23 23:33:18 +0100

    recommended simulink registration file.

--- Simulink/darwinoplib/blocks/slblocks.m
+++ Simulink/darwinoplib/blocks/slblocks.m
@@ -1,6 +1,13 @@
 function blkStruct = slblocks
 
-
+% Information for "Blocksets and Toolboxes" subsystem
 blkStruct.Name = 'DarwinOP Lib';
 blkStruct.OpenFcn = 'lib_darwinop';
 blkStruct.MaskInitialization = '';
+blkStruct.MaskDisplay = 'disp(''DarwinOP'')';
+
+% Information for Simulink Library Browser
+Browser(1).Library = 'lib_darwinop';
+Browser(1).Name = 'DarwinOP Lib';
+Browser(1).IsFlat = 1;% Is this library "flat" (i.e. no subsystems)?
+blkStruct.Browser = Browser;
