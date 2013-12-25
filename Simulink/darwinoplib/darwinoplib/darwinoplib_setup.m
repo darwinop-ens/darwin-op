690bde6c5f89b9c4e6222bf38d0f7dc107d4bf08
Florent Ouchet <florent.ouchet@bretagne.ens-cachan.fr>
2013-12-25 11:21:28 +0100

    first tests of code generation (not working yet).

--- Simulink/darwinoplib/darwinoplib/darwinoplib_setup.m
+++ Simulink/darwinoplib/darwinoplib/darwinoplib_setup.m
@@ -2,9 +2,11 @@ function darwinoplib_setup()
 
 curpath = pwd;
 tgtpath = curpath(1:end-length('\darwinoplib'));
+addpath(tgtpath);
 addpath(fullfile(tgtpath, 'darwinoplib'));
 addpath(fullfile(tgtpath, 'blocks'));
 addpath(fullfile(tgtpath, 'darwinoplibdemos'));
+addpath(fullfile(tgtpath, 'putty'));
 
 savepath;
 
