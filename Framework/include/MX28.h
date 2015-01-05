3f930ca39648578629ea8eb750eee2c9ac654bb8
Florent Ouchet <florent.ouchet@bretagne.ens-cachan.fr>
2015-01-05 15:16:36 -0500

    style cleaning.

--- Framework/include/MX28.h
+++ Framework/include/MX28.h
@@ -32,7 +32,7 @@ namespace Robot
 		static const double RATIO_VALUE2TORQUE = 0.01;
 		static const double RATIO_TORQUE2VALUE = 100;
 
-        	static int GetMirrorValue(int value)		{ return MAX_VALUE + 1 - value; }
+		static int GetMirrorValue(int value)		{ return MAX_VALUE + 1 - value; }
 		static double GetMirrorAngle(double angle)	{ return -angle; }
 
 		static int Angle2Value(double angle) { return (int)(angle*RATIO_ANGLE2VALUE)+CENTER_VALUE; }
@@ -50,12 +50,12 @@ namespace Robot
 		*/
 		enum
 		{
-			P_MODEL_NUMBER_L			= 0,
-			P_MODEL_NUMBER_H			= 1,
-			P_VERSION					= 2,
-			P_ID						= 3,
-			P_BAUD_RATE					= 4,
-			P_RETURN_DELAY_TIME			= 5,			
+			P_MODEL_NUMBER_L            = 0,
+			P_MODEL_NUMBER_H            = 1,
+			P_VERSION                   = 2,
+			P_ID                        = 3,
+			P_BAUD_RATE                 = 4,
+			P_RETURN_DELAY_TIME         = 5,
 			P_CW_ANGLE_LIMIT_L          = 6,
 			P_CW_ANGLE_LIMIT_H          = 7,
 			P_CCW_ANGLE_LIMIT_L         = 8,
@@ -66,7 +66,7 @@ namespace Robot
 			P_HIGH_LIMIT_VOLTAGE        = 13,
 			P_MAX_TORQUE_L              = 14,
 			P_MAX_TORQUE_H              = 15,
-			P_RETURN_LEVEL				= 16,
+			P_RETURN_LEVEL              = 16,
 			P_ALARM_LED                 = 17,
 			P_ALARM_SHUTDOWN            = 18,
 			P_OPERATING_MODE            = 19,
@@ -94,30 +94,30 @@ namespace Robot
 			P_PRESENT_LOAD_H            = 41,
 			P_PRESENT_VOLTAGE           = 42,
 			P_PRESENT_TEMPERATURE       = 43,
-			P_REGISTERED_INSTRUCTION	= 44,
+			P_REGISTERED_INSTRUCTION    = 44,
 			P_PAUSE_TIME                = 45,
-			P_MOVING					= 46,
-			P_LOCK						= 47,
-			P_PUNCH_L					= 48,
-			P_PUNCH_H					= 49,
+			P_MOVING                    = 46,
+			P_LOCK                      = 47,
+			P_PUNCH_L                   = 48,
+			P_PUNCH_H                   = 49,
 			P_RESERVED4                 = 50,
-			    P_RESERVED5                 = 51,
-			    P_POT_L                     = 52,
-			    P_POT_H                     = 53,
-			    P_PWM_OUT_L                 = 54,
-			    P_PWM_OUT_H                 = 55,
-			    P_P_ERROR_L                 = 56,
-			    P_P_ERROR_H                 = 57,
-			    P_I_ERROR_L                 = 58,
-			    P_I_ERROR_H                 = 59,
-			    P_D_ERROR_L                 = 60,
-			    P_D_ERROR_H                 = 61,
-			    P_P_ERROR_OUT_L             = 62,
-			    P_P_ERROR_OUT_H             = 63,
-			    P_I_ERROR_OUT_L             = 64,
-			    P_I_ERROR_OUT_H             = 65,
-			    P_D_ERROR_OUT_L             = 66,
-			    P_D_ERROR_OUT_H             = 67,
+			P_RESERVED5                 = 51,
+			P_POT_L                     = 52,
+			P_POT_H                     = 53,
+			P_PWM_OUT_L                 = 54,
+			P_PWM_OUT_H                 = 55,
+			P_P_ERROR_L                 = 56,
+			P_P_ERROR_H                 = 57,
+			P_I_ERROR_L                 = 58,
+			P_I_ERROR_H                 = 59,
+			P_D_ERROR_L                 = 60,
+			P_D_ERROR_H                 = 61,
+			P_P_ERROR_OUT_L             = 62,
+			P_P_ERROR_OUT_H             = 63,
+			P_I_ERROR_OUT_L             = 64,
+			P_I_ERROR_OUT_H             = 65,
+			P_D_ERROR_OUT_L             = 66,
+			P_D_ERROR_OUT_H             = 67,
 			MAXNUM_ADDRESS
 		};
 	};
